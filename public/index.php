2c568ba07f6cdad0824305a50b6370977d27fa80
Peter Suhm <peter@suhm.dk>
2015-01-03 09:50:16 +0100

    tear it down

--- public/index.php
+++ public/index.php
@@ -1,43 +1,65 @@
-<?php
+<?php date_default_timezone_set('Europe/Copenhagen'); ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@petersuhm</title>
 
-require '../vendor/autoload.php';
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+        <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Lato' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
 
-date_default_timezone_set('Europe/Copenhagen');
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+            <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class="container">
 
-$app = new \Slim\Slim();
+            <h1 class="really-large-text">Peter Suhm</h1>
 
-$app->config(array(
-    'templates.path' => '../templates'
-));
+            <ul class="hashtags">
+                <li><a href="#/freelancer">#developer</a></li>
+                <li><a href="#/projects">#entrepreneur</a></li>
+            </ul>
 
-$app->view(new \Slim\Views\Twig());
+            <ul class="hashtags small">
+                <li><a href="http://tutsplus.com/authors/petersuhm" target="_blank">#writer</a></li>
+                <li><a href="http://abroadness.com/" target="_blank">#traveler</a></li>
+                <li><a href="http://suhm.dk/" target="_blank">#danish</a></li>
+            </ul>
 
-$app->postLoader = new \Thin\PostLoader;
-$app->postLoader->config(array(
-    'document_path' => '../content',
-    'document_ext' => '.md'
-));
+            <ul class="icons">
+                <li><a href="https://github.com/petersuhm" target="_blank"><i class="fa fa-github fa-3x"></i></a></li>
+                <li><a href="https://twitter.com/petersuhm" target="_blank"><i class="fa fa-twitter fa-3x"></i></a></li>
+                <li><a href="mailto:peter@suhm.dk"><i class="fa fa-envelope fa-3x"></i></a></li>
+            </ul>
 
-$app->get('/', function () use ($app)
-{
-    $posts = $app->postLoader->all();
+            <div class="tweets">
+                <a class="twitter-timeline" href="https://twitter.com/petersuhm" data-widget-id="551124394681331712">Tweets by @petersuhm</a>
+                <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+            </div>
 
-    $app->render('index.html', array('posts' => $posts));
-});
+        </div>
 
-$app->get('/blog', function () use ($app)
-{
-    $posts = $app->postLoader->all();
+        <footer>
+            <div class="container">
+                <small>
+                    Copyright &copy; <?php echo date('Y'); ?> <a href="http://petersuhm.com">Peter Suhm</a>
+                    | <a href="mailto:peter@suhm.dk">peter@suhm.dk</a>
+            </div>
+        </footer>
 
-    $app->render('blog.html', array('posts' => $posts));
-});
-
-$app->get('/blog/:slug', function ($slug) use ($app)
-{
-    $post = $app->postLoader->find($slug);
-    $parser = new \Thin\Parsers\MarkdownExtraParser;
-
-    $app->render('post.html', array('post' => $post, 'parser' => $parser));
-});
-
-$app->run();
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+    </body>
+</html>
